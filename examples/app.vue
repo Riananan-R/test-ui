d77e30acb687e9fd6a87d12628e40894ef523282
友人A <niu13259716901@163.com>
2024-11-26 10:46:38 +0800

    feat: Complate Message Component

--- examples/app.vue
+++ examples/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <!-- button -->
+    <h2>按钮</h2>
     <h3>Button</h3>
     <div>
       <t-button>default</t-button>
@@ -58,17 +59,42 @@
     <div>
       <t-button type="primary" :loading="loading">加载中</t-button>
     </div>
+    <!-- message -->
+    <h2>消息</h2>
+    <h3>Message</h3>
+    <t-button type="success" @click="handleMessage('success')">success</t-button>
+    <t-button type="info" @click="handleMessage('info')">info</t-button>
+    <t-button type="warning" @click="handleMessage('warning')">warning</t-button>
+    <t-button type="danger" @click="handleMessage('error')">danger</t-button>
+    <h3>Message close</h3>
+    <t-button type="success" @click="handleMessage('success', true)">success</t-button>
+    <t-button type="info" @click="handleMessage('info', true)">info</t-button>
+    <t-button type="warning" @click="handleMessage('warning', true)">warning</t-button>
+    <t-button type="danger" @click="handleMessage('error', true)">danger</t-button>
+    <h3>Message time</h3>
+    <t-button type="success" @click="handleMessage('success', true, 1000)">1s</t-button>
+    <t-button type="success" @click="handleMessage('success', true, 5000)">5s</t-button>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { TMessage } from "@test-ui/components";
 
 const loading = ref(true);
 
 setTimeout(() => {
   loading.value = false;
 }, 3000);
+
+const handleMessage = (type, showClose, time) => {
+  TMessage({
+    type,
+    message: type,
+    showClose,
+    duration: time,
+  });
+};
 </script>
 
 <style scoped></style>
